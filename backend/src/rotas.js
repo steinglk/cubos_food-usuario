519dab7f775d2e218b46c97c4d6a9194218315d8
Matheus Stein <soloqstein@outlook.com>
2021-08-11 20:18:05 -0300

    Cadastro e filtro de login implementados

--- backend/src/rotas.js
+++ backend/src/rotas.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const cliente = require('./controladores/cliente');
+const login = require('./controladores/login');
+const verificarLogin = require('./filtros/verificaLogin/index');
 
 const rotas = express();
 
 rotas.post('/cadastro', cliente.cadastrarCliente);
+
+rotas.post('/', login.logarCliente);
+
+rotas.use(verificarLogin.verificarLogin);
+
+
 
 module.exports = rotas;
