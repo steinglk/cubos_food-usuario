b734bf007c9148f290d991fe7ada165767967373
Matheus Stein <soloqstein@outlook.com>
2021-08-11 09:42:14 -0300

    estrutura inicial criada

---
+++ frontend/consumidor-app/src/routes.js
@@ -0,0 +1,51 @@
+import React, { createContext, useContext, useState } from 'react';
+import {
+    BrowserRouter as Router,
+    Redirect,
+    Route,
+    Switch
+} from 'react-router-dom';
+
+import Login from './pages/Login';
+import Cadastro from './pages/Cadastro';
+import Dashboard from './pages/Dashboard';
+
+export const AuthContext = createContext();
+
+function RotasProtegidas(props) {
+    const { token } = useContext(AuthContext);
+
+    return(
+        <Route render={() => (localStorage.getItem('@restaurante/token') ? props.children : <Redirect to='/' />)} />
+    )
+}
+
+function Routes() {
+    const [token, setToken] = useState('');
+
+    function logar(tokenLogar) {
+        setToken(tokenLogar);
+        
+    }
+
+    function deslogar() {
+        setToken('');
+    }
+
+    return (
+        <AuthContext.Provider value={{ token, logar, deslogar }}>
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={Login} />
+                    <Route path="/cadastro" component={Cadastro} />
+                    <RotasProtegidas>
+                        <Route path="/produtos" component={Dashboard}/>
+                    </RotasProtegidas>
+                </Switch>
+            </Router>
+        </AuthContext.Provider>
+
+    );
+}
+
+export default Routes;
