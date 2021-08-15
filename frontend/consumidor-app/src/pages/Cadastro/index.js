7e90e9a1a68822dcbb297501b6241a9203f43100
zira-ell <niltonmenezes@live.com>
2021-08-15 12:58:42 -0300

    Finalização da página de cadastro

--- frontend/consumidor-app/src/pages/Cadastro/index.js
+++ frontend/consumidor-app/src/pages/Cadastro/index.js
@@ -17,14 +17,12 @@ import {AuthContext} from '../../routes';
 
 function Cadastro() {
     const [showPassword, setShowPassword] = useState(false);
-    const [confirmShowPassword, setConfirmShowPassword] = useState(false);
     const {register, handleSubmit} = useForm();
-    const {logar} = useContext(AuthContext);
     const history = useHistory();
 
     async function onSubmit(data) {
-        if (!data.email || !data.senha || !data.telefone || !data.senha || data.confirmarSenha) {
-            toast.error('Todos os são obrigatórios!', {
+        if (!data.email || !data.senha || !data.telefone || !data.senha || !data.confirmarSenha) {
+            toast.error('Todos os campos são obrigatórios!', {
                 position: "top-right",
                 autoClose: 3000,
                 hideProgressBar: true,
@@ -36,8 +34,20 @@ function Cadastro() {
             return;
         }
 
+        if (data.senha !== data.confirmarSenha) {
+            toast.error('As senhas não correspodem.', {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined
+            });
+            return;
+        }
 
-        const resposta = await fetch('http://localhost:8000/cadastro', {
+        const resposta = await fetch('http://localhost:5000/cadastro', {
             method: 'POST',
             body: JSON.stringify(data),
             headers: {
@@ -45,6 +55,32 @@ function Cadastro() {
             }
         });
         
+        const retorno = await resposta.json();
+        
+        if (retorno === "Email já cadastrado"){
+            toast.error('E-mail já cadastrado.', {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined
+            });
+            return;
+        }
+
+        if (retorno === "Cadastro realizado com sucesso"){
+            toast.success('Cadastro realizado com sucesso!.', {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined
+            });
+        }
 
         history.push('/');
     }
@@ -77,11 +113,49 @@ function Cadastro() {
                     <input className='input-cadastro' id='email' type='email' {... register('email')}/>
                 </div>
                 <div className='flex-column mt'>
-                    <label htmlFor='Telefone' className='label-cad'>Telefone</label>
-                    <input className='input-cadastro' id='Telefone' type='Telefone' {... register('email')}/>
+                    <label htmlFor='telefone' className='label-cad'>Telefone</label>
+                    <input className='input-cadastro' id='telefone' type='Telefone' {... register('telefone')}/>
                 </div>
-                    <div className="flex-column input-password">
-                        <label htmlFor="senha">Senha</label>
+
+                    <div className="flex-column input-password mt">
+                        <label htmlFor='senha' className='label-cad'>Senha</label>
+                        <input id='senha' 
+                            className="input-cadastro input-relative"
+                            name="senha"
+                            {...register('senha')}
+                            type={showPassword ? 'text' : 'password'} 
+                        />
+                        <FontAwesomeIcon 
+                            icon={showPassword ? faEye : faEyeSlash} className="eye-password"
+                            size="lg"
+                            onClick={() => setShowPassword(!showPassword)}
+                        />
+                    </div>
+
+                    <div className="flex-column input-password mt">
+                        <label htmlFor='confirmarSenha' className='label-cad'>Repita a sua Senha</label>
+                        <input id='confirmarSenha' 
+                            className="input-cadastro input-relative"
+                            name="confirmarSenha"
+                            {...register('confirmarSenha')}
+                            type={showPassword ? 'text' : 'password'} 
+                        />
+                        <FontAwesomeIcon 
+                            icon={showPassword ? faEye : faEyeSlash} className="eye-password"
+                            size="lg"
+                            onClick={() => setShowPassword(!showPassword)}
+                        />
+                    </div>
+
+                    <div className='flex-row content-center'>
+                        <button className='btn-orange button-margin mt'type='submit' >Criar conta</button>
+                    </div>
+                    <div className='text-center'>
+                        <span className="span-login">Já tem uma conta?</span>
+                        <Link to='/cadastro' className='link-login'>Login</Link>
+                    </div>
+
+                        {/* <label htmlFor="senha">Senha</label>
                         <input className="input-cadastro"
                             type={showPassword ? 'text' : 'password'}
                             name="senha"
@@ -89,8 +163,8 @@ function Cadastro() {
                         />
                         <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} className="eye-password"
                             size="lg"
-                            onClick={() => setShowPassword(!showPassword)} />
-                    </div>
+                            onClick={() => setShowPassword(!showPassword)} /> */}
+                    
                 </form>
             </div>
         </div>
