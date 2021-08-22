f9b8c5e58ed40dbc501e6469eecfc7d019044dbc
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2021-08-21 21:11:16 -0300

    Adicionando rota de cadastrar e visualizar endereço do cliente

--- backend/src/controladores/cliente/index.js
+++ backend/src/controladores/cliente/index.js
@@ -9,13 +9,13 @@ const cadastrarCliente = async (req, res) => {
         telefone,
     } = req.body;
 
-    if(!nome.trim() || !email.trim() || !senha.trim() || !telefone.trim()) {
+    if (!nome.trim() || !email.trim() || !senha.trim() || !telefone.trim()) {
         return res.status(400).json("Todos os campos são obrigatórios");
     }
     try {
         const clienteExiste = await knex('cliente').select('*').where({ email }).first();
 
-        if(clienteExiste) {
+        if (clienteExiste) {
             return res.status(400).json("Email já cadastrado");
         }
         const senhaCriptografada = await bcrypt.hash(senha, 10);
@@ -27,7 +27,7 @@ const cadastrarCliente = async (req, res) => {
                 telefone,
                 senha: senhaCriptografada,
             });
-        if(cadastrandoCliente.length === 0) {
+        if (cadastrandoCliente.length === 0) {
             return res.status(400).json("Erro ao realizar cadastro");
         }
         return res.status(200).json('Cadastro realizado com sucesso');
@@ -36,7 +36,87 @@ const cadastrarCliente = async (req, res) => {
     }
 }
 
+const adicionarEndereco = async (req, res) => {
+    const {
+        cliente_id,
+        cep,
+        endereco,
+        complemento,
+    } = req.body;
+
+    if (!cliente_id || !cep || !endereco || !complemento) {
+        return res.status(400).json("Todos os campos são obrigatórios");
+    }
+
+    try {
+        const clienteExiste = await knex('cliente')
+            .select('id')
+            .where({ id: cliente_id })
+            .first();
+
+        if (!clienteExiste) {
+            return res.status(400).json("O cliente informado ainda não possui um cadastro em nosso sistema");
+        }
+
+        const enderecoExiste = await knex('endereco_cliente')
+            .select('id')
+            .where({ id: cliente_id })
+            .first();
+
+        if (enderecoExiste) {
+            const atualizandoEndereco = await knex('endereco_cliente')
+                .where({ id: cliente_id })
+                .update({
+                    cep,
+                    endereco,
+                    complemento,
+                });
+
+            if (!atualizandoEndereco) {
+                return res.status(400).json("Erro ao atualizar endereço!");
+            }
+
+            return res.status(200).json('Endereço atualizado com sucesso');
+        }
+
+        const cadastrandoEndereco = await knex('endereco_cliente')
+            .insert({
+                cliente_id,
+                cep,
+                endereco,
+                complemento,
+            });
+        if (cadastrandoEndereco.length === 0) {
+            return res.status(400).json("Erro ao adicionar endereço!");
+        }
+        return res.status(200).json('Endereço adicionado com sucesso');
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
+}
+
+const verificarEndereco = async (req, res) => {
+    
+    const { id } = req.params;
+
+    try {
+        const enderecoEncontrado = await knex('endereco_cliente')
+            .select('endereco', 'complemento', 'cep')
+            .where({ cliente_id: id })
+            .first();
+
+        if (!enderecoEncontrado) {
+            return res.status(400).json("O cliente não possui um endereço cadastrado em nosso sistema");
+        }
+
+        return res.status(200).json(enderecoEncontrado);
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
+}
 
 module.exports = {
-    cadastrarCliente
+    cadastrarCliente,
+    adicionarEndereco,
+    verificarEndereco
 }
